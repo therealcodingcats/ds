8403112f370049ddd480525ebffbb02789658e97
donglt <donglt@rabiloo.com.vn>
2023-01-30 11:54:41 +0700

    init

---
+++ Runtime/singleton/SingletonBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace ds
+{
+    public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        [CanBeNull] private static T _instance;
+
+        [CanBeNull]
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+
+                var allObjectsWithType = FindObjectsOfType<T>();
+                if (allObjectsWithType.Length == 0) return null;
+
+                _instance = allObjectsWithType[0];
+
+                return _instance;
+            }
+        }
+    }
+}
