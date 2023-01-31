86db23460268a71c97c9b6e95b3fcba89da18f0e
donglt <donglt@rabiloo.com.vn>
2023-01-31 16:02:49 +0700

    add Collection utilities

---
+++ Runtime/Collections.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ds
+{
+    public static class Collections
+    {
+        public static Optional<V> Get<K, V>(this IReadOnlyDictionary<K, V> dictionary, K key) where V : class =>
+            Optional<V>.Of(dictionary.ContainsKey(key) ? dictionary[key] : null);
+
+        public static bool IsEmpty<T>(this IReadOnlyCollection<T> list) => list.Count == 0;
+        public static bool IsEmpty<T>(this ICollection<T> list) => list.Count == 0;
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var element in source) action.Invoke(element);
+        }
+    }
+}
